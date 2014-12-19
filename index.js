6189a88cd979d56bdcd673b9e287f648e5ad4b25
oeuillot <olivier.oeuillot@gmail.com>
2014-12-19 11:11:38 +0100

    First version

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 var express = require('express');
 var OMXControl = require('omxcontrol');
 
-var omx = new OMXControl({
+var MOVIES_PATH = "/home/olivier/Films";
 
+var omx = new OMXControl({
+	moviesPath: MOVIES_PATH
 });
 
 var app = express();
 app.use(omx.express);
+
+app.get("/list/:path", function(req, res) {
+
+	var path = MOVIES_PATH + "/" + req.attrs.path;
+	if (!fs.existsSync(path)) {
+		res.status(404);
+		res.end();
+		return;
+	}
+
+	var hs = {
+		'Content-Type': 'application/json',
+		'Cache-Control': NO_CACHE_CONTROL
+	};
+	res.writeHead(200, hs);
+
+	res.write('{ "responseCode": "OK", "list": [');
+
+	fs.readdirSync(path).forEach(function(p) {
+		var stats = fs.statSync(p);
+		if (!stats) {
+			return;
+		}
+
+		if (stats.isDirectory()) {
+			res.write('/' + p);
+			return;
+		}
+
+		res.write(p);
+	});
+
+	res.write(']}');
+	res.end();
+});
 
 app.use(express.static(__dirname + '/pages'));
 

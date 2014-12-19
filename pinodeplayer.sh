34f454993bafca0d745bff01cafde8ae01f96cf3
oeuillot <olivier.oeuillot@gmail.com>
2014-12-19 16:23:32 +0100

    Try auto restart

--- pinodeplayer.sh
+++ pinodeplayer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-until node /home/olivier/sources/pinodeplayer/index.js; do
+cd /home/olivier/sources/pinodeplayer
+
+until node index.js; do
     echo "PiNodePlayer crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done

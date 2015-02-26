c83796fd9979e75185eaa9f3a89e106929099184
oeuillot <olivier.oeuillot@gmail.com>
2015-02-26 17:28:25 +0100

    misc

--- pi-nodeplayer.sh
+++ pi-nodeplayer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-until node index.js; do
+MOVIES_PATH="$1";
+
+until node index.js -o hdmi -p --moviesBasePath $MOVIES_PATH; do
     echo "pi-nodeplayer crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
